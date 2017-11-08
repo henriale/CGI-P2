b2c7ac1c593ab2f1932d1783139dcd747458ace4
Alexandre Araujo <alexandreh.araujo@gmail.com>
2017-11-08 11:37:11 -0200

    hopefully it's working 🙏

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# CGI-P2
+# COMPUTAÇÃO GRÁFICA I - ALEXANDRE ARAUJO
+
+## OBJETIVO:
+
+Coletar ao menos 3 teapots em cada fase.
+Ao total, são 29 fases.
+
+## CONTROLE:
+
+- Setas direcionais para mover o personagem
+- **R**: Reinicia o jogo
+- **Esc**: Fecha o jogo
+
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ ESCAPE = b'\x1b'
 window = 0
 scenario_size = 60
 bullets = []
+score = 0
 elapsed_time = 0
 
 # rotation
@@ -18,17 +19,12 @@ X_AXIS = 0.0
 Z_AXIS = 0.0
 Y_AXIS = 0.0
 
-MAIN_X = 0
-MAIN_Y = 0
-MAIN_Z = 0
-# camera
-OBS_X = 0.0
-OBS_Y = 0.0
-OBS_Z = 0.0
+obs_x = 0
+obs_y = 0
+obs_z = 0
 
-
-
-DIRECTION = 1
+level = 0
+game_over = False
 
 class Vertex:
     def __init__(self, x, y, z):
@@ -45,11 +41,9 @@ class RGB:
 
 
 def main():
-    global window, bullets, elapsed_time
+    global window, elapsed_time
 
-    file = open("./dataset/BR-01.txt")
-
-    bullets = normalize_dataset(*read_dataset(file))
+    next_level()
 
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
@@ -59,15 +53,26 @@ def main():
     window = glutCreateWindow('CGI-P2')
 
     elapsed_time = glutGet(GLUT_ELAPSED_TIME)
-    glutDisplayFunc(DrawGLScene)
-    glutIdleFunc(DrawGLScene)
-    glutKeyboardFunc(keyPressed)
-    glutSpecialFunc(specialKeyPressed)
-    InitGL(640, 480)
+    glutDisplayFunc(draw_scene)
+    glutIdleFunc(draw_scene)
+    glutKeyboardFunc(key_pressed)
+    glutSpecialFunc(special_key_pressed)
+
+    init(640, 480)
     glutMainLoop()
 
 
-def InitGL(Width, Height):
+def next_level():
+    global bullets, score, level
+
+    score = 0
+    level += 1
+
+    file = open("./dataset/%02d.txt" % level)
+    bullets = normalize_dataset(*read_dataset(file))
+
+
+def init(width, height):
     glClearColor(0.0, 0.0, 0.0, 0.0)
     glClearDepth(1.0)
     glDepthFunc(GL_LESS)
@@ -75,39 +80,51 @@ def InitGL(Width, Height):
     glShadeModel(GL_SMOOTH)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-    gluPerspective(45.0, float(Width) / float(Height), 0.1, 100.0)
+    gluPerspective(45.0, float(width) / float(height), 0.1, 100.0)
     glMatrixMode(GL_MODELVIEW)
 
 
-def keyPressed(*args):
+def key_pressed(*args):
+    global bullets,score,obs_x,obs_y,obs_z,level,game_over
+
+    if args[0] == b'r':
+        bullets = []
+        score = 0
+        obs_x = 0
+        obs_y = 0
+        obs_z = 0
+        level = 0
+        game_over = False
+        next_level()
+
     if args[0] == ESCAPE:
         sys.exit()
 
 
-def specialKeyPressed(*args):
-    global MAIN_X, MAIN_Y, MAIN_Z, X_AXIS, Z_AXIS, Y_AXIS
+def special_key_pressed(*args):
+    global obs_x, obs_y, obs_z, X_AXIS, Z_AXIS, Y_AXIS
 
     if args[0] == GLUT_KEY_LEFT:
-        Y_AXIS = (Y_AXIS + 45) % 360
+        #Y_AXIS = (Y_AXIS + 45) % 360
+        obs_x += 1
 
     if args[0] == GLUT_KEY_RIGHT:
-        Y_AXIS = (Y_AXIS - 45) % 360
+        #Y_AXIS = (Y_AXIS - 45) % 360
+        obs_x -= 1
 
     if args[0] == GLUT_KEY_UP:
-        MAIN_Z += 1
+        obs_z += 1
 
     if args[0] == GLUT_KEY_DOWN:
-        MAIN_Z -= 1
+        obs_z -= 1
 
     glutPostRedisplay()
 
 
-def DrawGLScene():
-    global OBS_X, OBS_Y, OBS_Z
-    global DIRECTION
+def draw_scene():
     global scenario_size
     global elapsed_time
-    global MAIN_X, MAIN_Z
+    global obs_x, obs_z
     global X_AXIS, Y_AXIS, Z_AXIS
 
     curr_time = glutGet(GLUT_ELAPSED_TIME)
@@ -117,20 +134,23 @@ def DrawGLScene():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
     glLoadIdentity()
-    glTranslatef(0.0, -5.0, -30.0)
+    glRotatef(-Y_AXIS, 0.0, 1.0, 0.0)
+    gluLookAt(obs_x - 20, 10, obs_z - 20, obs_x, 1, obs_z, 0.0, 1.0, 0.0)
 
-    gluLookAt(OBS_X, 1, OBS_Z, OBS_X, 1, OBS_Z + 1, 0.0, 1.0, 0.0)
+    printText(obs_x, 2, obs_z, 'Teapots left: %d' % len(bullets))
+    printText(obs_x, 3, obs_z, 'Score: %s' % score)
+    printText(obs_x, 4, obs_z, 'Level: %02d' % level)
 
-    drawMainCharacter(position=Vertex(MAIN_X, 1, MAIN_Z), size=2)
+    draw_main_character(position=Vertex(obs_x, 1, obs_z), size=2)
 
-    drawBullets()
+    draw_bullets()
 
-    drawGround(sqm=1, size=scenario_size)
+    draw_ground(sqm=1, size=scenario_size)
 
     glutSwapBuffers()
 
 
-def drawGround(sqm=10, size=1000, color=None):
+def draw_ground(sqm=10, size=1000, color=None):
     if color is None:
         color = RGB(0.2, 0.1, 0.2)
 
@@ -150,22 +170,37 @@ def drawGround(sqm=10, size=1000, color=None):
     glLineWidth(1)
 
 
-
-def drawBullets():
-    global bullets
+def draw_bullets():
+    global bullets, score, obs_x, obs_z, game_over
 
     if not bullets:
-        return
+        if score < 3:
+            game_over = True
+
+        if game_over:
+            printText(obs_x, 5, obs_z, 'GAME OVER!')
+            return
+        else:
+            next_level()
 
     if not bullets[0]:
         del bullets[0]
-        return drawBullets()
+        return draw_bullets()
 
     (x, z) = bullets[0].pop()
-    drawBullet(position=Vertex(x, 1, z), size=1)
+
+    diffX = obs_x - x
+    diffZ = obs_z - z
+
+    if abs(diffX) < 1 or abs(diffZ) < 1:
+        print(abs(diffX), abs(diffZ))
+        score += 1
+        del bullets[0]
+
+    draw_bullet(position=Vertex(x, 1, z), size=1)
 
 
-def drawMainCharacter(size=2, position=None, color=None):
+def draw_main_character(size=2, position=None, color=None):
     global X_AXIS, Z_AXIS, Y_AXIS
     half = size / 2
 
@@ -180,12 +215,12 @@ def drawMainCharacter(size=2, position=None, color=None):
     glRotatef(Y_AXIS, 0.0, 1.0, 0.0)
     glTranslatef(position.x, position.y, position.z)
     glColor3f(color.r, color.g, color.b)
-    glutSolidCone(half, 5, 50, 50)
+    glutSolidSphere(half, 5, 50, 50)
 
     glPopMatrix()
 
 
-def drawBullet(size=2, position=None, color=None):
+def draw_bullet(size=2, position=None, color=None):
     half = size / 2
 
     if color is None:
@@ -197,7 +232,7 @@ def drawBullet(size=2, position=None, color=None):
     glPushMatrix()
     glTranslatef(position.x, position.y, position.z)
     glColor3f(color.r, color.g, color.b)
-    glutSolidSphere(half, 50, 50)
+    glutSolidTeapot(half, 50, 50)
     glPopMatrix()
 
 
@@ -253,27 +288,12 @@ def read_dataset(file):
     return bxs, max_x, max_z, min_x, min_z
 
 
-def printText(x, y, message):
-    glDisable(GL_TEXTURE_2D)
-    glMatrixMode(GL_PROJECTION)
-    glPushMatrix()
-    glLoadIdentity()
-    gluOrtho2D(0.0, 640, 0.0, 480)
-    glMatrixMode(GL_MODELVIEW)
-    glPushMatrix()
-    glLoadIdentity()
-    glRasterPos2i(10, 10)
+def printText(x, y, z, message):
+    glColor3d(1, 1, 1)
 
+    glRasterPos3d(x, y, z)
     for c in message:
-        glColor3d(1.0, 0.0, 0.0)
-        var = fonts.name
-        # glutBitmapCharacter(GLUT_BITMAP_9_BY_15, c)
-
-    glMatrixMode(GL_PROJECTION)
-    glPopMatrix()
-    glMatrixMode(GL_MODELVIEW)
-    glPopMatrix()
-    glEnable(GL_TEXTURE_2D)
+        glutBitmapCharacter(globals()['GLUT_BITMAP_HELVETICA_18'], ord(c))
 
 
 if __name__ == "__main__":
